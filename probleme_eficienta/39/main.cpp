ba2a1ad9ff5edfab4c68ea2ee26e198573df2bb0
zaBogdan <35929785+zaBogdan@users.noreply.github.com>
2020-02-28 14:39:49 +0200

    Update main.cpp

--- probleme_eficienta/39/main.cpp
+++ probleme_eficienta/39/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <fstream>
 #include <cmath>
 using namespace std;
-ifstream f("bac.in");
-
 
 int main(){
-
+    long long k;
+    cin >> k;
+    for(int i=1;i<=sqrt(k);i++){
+        if(k-i*i >= 0 && sqrt(k-i*i)==floor(sqrt(k-i*i)) && i<=sqrt(k-i*i))
+              cout << i << ' ' << sqrt(k-i*i) << endl;
+    }
     cout << endl; //development purpose.
     return 0;
-} 
+}
